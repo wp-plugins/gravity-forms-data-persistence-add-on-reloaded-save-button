f4092d71ea85a64086bea2cfa554eeb4bd368ab5
ovann86 <ovann86@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 23:55:45 +0000

    version 1.0 - First public release.
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-data-persistence-add-on-reloaded-save-button/trunk@1084195 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gravity-forms-data-persistence-add-on-reloaded-save-button.php
@@ -0,0 +1,151 @@
+<?php
+/*
+Plugin Name: Gravity Forms - Data Persistence Reloaded - Save Button
+Description: Extends the 'Gravity Forms - Data Persistence Reloaded' plugin to add a save button to the form footer - allowing users to instantly save their form, and providing the reassurance that the form has been saved.
+Version: 1.0
+Author: Adrian Gordon
+License: GPL2
+*/
+
+add_action('admin_notices', array('ITSP_GF_DPR_Save_Button', 'admin_warnings'), 20);
+
+if (!class_exists('ITSP_GF_DPR_Save_Button')) {
+    class ITSP_GF_DPR_Save_Button
+    {
+		private static $name = 'Gravity Forms - Data Persistence Reloaded - Save Button';
+		private static $slug = 'itsp_gp_drp_save_button';
+		
+		/**
+         * the save button template
+         */
+		public static $save_button =  "<input type='button' value='Save' class='button gform_next_button itsg_gf_save_button' onclick='itsg_gf_save_button_ajax_function();'> "; 
+
+        /**
+         * Construct the plugin object
+         */
+		 public function __construct()
+        {
+            // register actions
+            if ((self::is_gravityforms_installed())) {
+			// start the plugin
+			add_action('gform_enqueue_scripts', array(&$this,'itsg_gf_save_button_button_ajax'), 90, 3);
+			add_filter( 'gform_next_button', array(&$this,'my_next_button_markup'), 10, 2 );
+			add_filter( 'gform_submit_button', array(&$this,'my_submit_button_markup'), 10, 2 );
+			}
+        } // END __construct
+		
+		/**
+         * Place the save button before the 'Next' button
+         */
+		function my_next_button_markup( $next_button, $form ) {
+			$save_button = self::$save_button;
+			return $save_button.$next_button;
+		} // END my_next_button_markup
+		
+		/**
+         * Place the save button before the 'Submit' button
+         */
+		function my_submit_button_markup( $submit_button, $form ) {
+			$save_button = self::$save_button;
+			return $save_button.$submit_button;
+		} // END my_submit_button_markup
+
+		/**
+         * If Ajax data persistence is enabled, user is logged in - enqueue javascript
+         */
+		public function itsg_gf_save_button_button_ajax($form, $is_ajax) {
+			if($form['ri_gfdp_persist'] == 'ajax' && is_user_logged_in()) {
+			
+				add_action('wp_footer', array(&$this,'itsg_gf_save_button_ajax'));
+				
+			}
+		} // END itsg_gf_save_button_button_ajax
+		
+		/**
+         * Ajax to handle button press - 
+		 *     activates the 'save' command, 
+		 *     sets the button value to 'Saving ...' whilst being saved, 
+		 *     sets the button value to 'Saved' for three seconds to confirm to the user that the save completed,
+		 *     sets the button value back to 'Save'
+         */
+		public static function itsg_gf_save_button_ajax() {
+		?>
+				<script type="text/javascript" >
+				function itsg_gf_save_button_ajax_function() {
+				jQuery('.gform_body').find('.itsg_gf_save_button').prop('value', '<?php _e('Saving ...', self::$slug); ?>');
+				
+					changed = true; 
+					gfdp_ajax();
+					jQuery(document).ajaxComplete(function(event, request, settings ) {
+						if ( settings.url === '<?php echo admin_url('admin-ajax.php'); ?>' && request.responseText === 'Saved' ) {
+							itsg_gf_save_button_ajax_reset_button_function();
+						}
+					});
+				}
+								
+				function itsg_gf_save_button_ajax_reset_button_function() {
+					jQuery('.gform_body').find('.itsg_gf_save_button').prop('value', '<?php _e('Saved', self::$slug); ?>');
+					setTimeout(function(){
+						jQuery('.gform_body').find('.itsg_gf_save_button').prop('value', '<?php _e('Save', self::$slug); ?>');
+					}, 3000);
+				}
+				
+				</script> <?php
+		} // END itsg_gf_save_button_ajax
+
+		/*
+         * Warning message if Gravity Forms is not installed and enabled
+         */
+		public static function admin_warnings() {
+			if ( !self::is_gravityforms_installed() ) {
+				$gf_message = __('Requires Gravity Forms to be installed.', self::$slug);
+			}
+			if ( !self::is_dpr_installed() ) {
+				$gfdpr_message = __('Requires Gravity - Data Persistence Reloaded to be installed.', self::$slug);
+			}
+			
+			if (!empty($gf_message)) {
+			?>
+			<div class="error">
+				<p>
+					<?php _e('The plugin ', self::$slug); ?><strong><?php echo self::$name; ?></strong> <?php echo $gf_message; ?><br />
+					<?php _e('Please ',self::$slug); ?><a href="http://www.gravityforms.com/"><?php _e(' download the latest version',self::$slug); ?></a><?php _e(' of Gravity Forms and try again.',self::$slug) ?>
+				</p>
+			</div>
+			<?php
+			}
+			
+			if (!empty($gfdpr_message)) {
+			?>
+			<div class="error">
+				<p>
+					<?php _e('The plugin ', self::$slug); ?><strong><?php echo self::$name; ?></strong> <?php echo $gfdpr_message; ?><br />
+					<?php _e('Please ',self::$slug); ?><a href="https://wordpress.org/plugins/gravity-forms-data-persistence-add-on-reloaded/"><?php _e(' download the latest version',self::$slug); ?></a><?php _e(' of Gravity Forms Data Persistence Add-On Reloaded and try again.',self::$slug) ?>
+				</p>
+			</div>
+			<?php
+			} 
+			
+		} // END admin_warnings
+
+		/*
+         * Check if GF is installed
+         */
+        private static function is_gravityforms_installed()
+        {
+            return class_exists('GFAPI');
+        } // END is_gravityforms_installed
+		
+		/*
+         * Check if Gravity Forms - Data Persistence Reloaded is installed
+         */
+        private static function is_dpr_installed() 
+        {
+            return function_exists('ri_gfdp_ajax');
+        } // END is_dpr_installed
+		
+	}
+    $ITSP_GF_DPR_Save_Button = new ITSP_GF_DPR_Save_Button();
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Gravity Forms - Data Persistence Add-On Reloaded - Save Button ===
+Contributors: ovann86
+Donate link: http://www.itsupportguides.com/
+Tags: Gravity Forms, Gravity Forms Data Persistence Add-On Reloaded, addon
+Requires at least: 4.0
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A save button for Gravity Forms.
+
+== Description ==
+
+This plugin requires the [Gravity Forms](http://www.gravityforms.com/ "Gravity Forms website") and [Gravity Forms Data Persistence Add-On Reloaded plugins](https://wordpress.org/plugins/gravity-forms-data-persistence-add-on-reloaded/ "Gravity Forms Data Persistence Add-On Reloaded") plugins.
+
+With Gravity Forms Data Persistence Add-On Reloaded, when 'Save data with ajax' is enabled a form is automatically saved every ten seconds if a field has been changed. This leaves a gap of ten seconds where a users data may not be saved if they leave the page, it also doesnt provide any feedback to the user so they know their form has been saved successfully.
+
+This plugin adds a 'Save' button to the bottom of the form when 'Save data with ajax' is enabled for the form. Allowing logged on users to instantly save their form, and provides the reassurance that the form has been saved.
+
+To enable the 'Save' button, install and activate the plugin, open the form settings and under 'Persistence' choose 'Save data with ajax'.
+
+== Installation ==
+
+1. Install plugin from WordPress administration or upload folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in the WordPress administration
+1. Open the Gravity Forms 'Forms' menu
+1. Open the settings page for the form you want to add the 'Save' button to
+1. Under 'Persistence' choose 'Save data with ajax'
+1. The save button will now be added to the bottom of the form, along with the next, previous and submit buttons.
+
+== Screenshots ==
+
+1. Shows the 'Persistence' options found in the forms settings page. These options are provided by the Gravity Forms Data Persistence Add-On Reloaded plugin.
+2. Shows the 'Save' button on a single page form.
+3. Shows the 'Save' button on the first page of a multi-page form. The 'Save' and 'Next' buttons are displayed.
+4. Shows the 'Save' button on second page of a multi-page form. The 'Previous', 'Save' and 'Next' buttons are displayed.
+5. Shows the 'Save' button on third and final page of a multi-page form. The 'Previous', 'Save' and 'Submit' buttons are displayed.
+6. Shows the 'Save' button when the saving ajax is running. Whilst the form is being saved the button will display 'Saving ...'. In normal circumstances it is unlikely a user will see this, as the save command usually completes very quickly.
+7. After the form has been saved, the 'Save' button will display 'Saved'. The 'Saved' message is held for three seconds before returning to 'Save'.
+
+== Changelog ==
+
+= 1.0 =
+* First public release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+First public release.
